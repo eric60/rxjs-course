a40cd74d7ef402b63b52d006c7bd56a557b55c7b
Eric <ericshi98@gmail.com>
2025-08-15 09:57:21 -0400

    update lesson 1 - focus on streams before rxjs operators

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -26,8 +26,40 @@ import {createHttpObservable} from '../common/util';
 })
 export class AboutComponent implements OnInit {
 
-    ngOnInit() {
+    /*
+    stream of values
+    example: every click is a stream of values of the click event
 
+    almost everything is asynchronous in angular
+    1. requests from network bringing in data from backend,
+    2. timeouts in frontend
+    3. user interaction with clicks/mouse over events
+    need to combine all these async events in 1 program
+
+
+     */
+    ngOnInit() {
+        document.addEventListener('click', clickEvent => {
+          console.log(clickEvent);
+        })
+
+      // may use intervals with timeouts on frontend if doing long polling, waiting on backend response
+
+      let counter = 0
+      setInterval(() => {
+        console.log(counter)
+        counter++
+      }, 1000)
+
+      // now have 2 streams of values that might want to combine together (clicks + intervals)
+      // these are multi-value streams that continue to emit value over time and never complete
+      // Important: CAN they be completely, WHEN are they completed?
+
+
+      // setTimeout IS a special type of stream because it only contains and emits 1 value and then completes, similar to request to a backend that returns a values via a callback like ajax, with exception that setTimeout can't go wrong
+      setTimeout(() => {
+        console.log("setTimeout stream finished")
+      }, 3000)
 
     }
 

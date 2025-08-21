60e027985198f9b2313b339a2735e8955e4cf4bb
Eric <ericshi98@gmail.com>
2025-08-21 08:05:58 -0400

    export http$ into common function

--- src/app/common/util.ts
+++ src/app/common/util.ts
@@ -1,38 +1,35 @@
 import {Observable} from 'rxjs';
 
 
-export function createHttpObservable(url:string) {
-    return Observable.create(observer => {
-
-        const controller = new AbortController();
-        const signal = controller.signal;
-
-        fetch(url, {signal})
-            .then(response => {
-
-                if (response.ok) {
-                    return response.json();
-                }
-                else {
-                    observer.error('Request failed with status code: ' + response.status);
-                }
-            })
-            .then(body => {
-
-                observer.next(body);
-
-                observer.complete();
-
-            })
-            .catch(err => {
-
-                observer.error(err);
-
-            });
-
-        return () => controller.abort()
+// ================================================
+// ========= Custom http observable
+// ================================================
+//   promise much different than observable, promise executes immediately and only emits once or fails, observable only executes in response to a subscription instantiation click$.subscribe()
 
 
-    });
+// WHY? transform the fetch promise into an observable?
+//   advantage is can use ALL the rxjs operators to **COMBINE the httpstream with other stream of values** like clickhandlers, timeouts, other http requests
+
+// all observables like interval created like this
+// Creates a new cold Observable by calling the Observable constructor
+// cannot emit the http observable values inside create method, can only subscribe to it to get the stream of values, observer is private inside implementation of observable
+export function createHttpObservable(url: string): Observable<any> {
+  return Observable.create(observer => {
+    // observer.next();
+    // observer.error();
+    // observer.complete()
+    fetch('/api/courses')
+      .then(httpresponse => {
+        return httpresponse.json()
+      })
+      .then(jsonBody => {
+        observer.next(jsonBody) // actually emit the jsonbody value
+        observer.complete() // terminate http stream
+        // observer.next() WARNING: breaks observable contract!
+      })
+      .catch(err => {
+        observer.error(err)
+      })
+  })
 }
 

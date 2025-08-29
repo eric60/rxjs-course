1cc4ec52bcd6d078334b04112abb4472625c9b0c
Eric <ericshi98@gmail.com>
2025-08-29 18:42:01 -0400

    change from imperative to reactive streams http$.pipe(map(courses=>courses.filter()

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Course} from "../model/course";
 import {interval, noop, Observable, of, throwError, timer} from 'rxjs';
-import {catchError, delay, delayWhen, finalize, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
+import {catchError, delay, delayWhen, filter, finalize, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
 import {createHttpObservable} from '../common/util';
 import {Store} from '../common/store.service';
 
@@ -14,9 +14,12 @@ import {Store} from '../common/store.service';
 })
 export class HomeComponent implements OnInit {
 
+  // OLD WAY
   beginnerCourses: Course[];
-
   advancedCourses: Course[];
+
+  beginnerCourses$: Observable<Course[]>
+  advancedCourses$: Observable<Course[]>
 
   constructor() {
 
@@ -36,7 +39,8 @@ export class HomeComponent implements OnInit {
     // Option 1) Imperative option) subscribe and getting and setting data within it
     // Simplest way to get and set courses is to just simply subscribe to the courses$ and in the success callback just get the data and set the courses and pass back to template
     // problem is putting all the logic in the subscribe block will not scale with complexity, will quickly run into problem of nested subscribe calls (rxjs anti pattern) within each other causing callback hell,
-    courses$.subscribe(
+
+/*    courses$.subscribe(
       courses => {
         console.log("courses:", courses)
         this.beginnerCourses = courses.filter(course => course.category == 'BEGINNER')
@@ -44,10 +48,12 @@ export class HomeComponent implements OnInit {
       },
       noop, // ()  => {},
       () => console.log("courses$ completed")
-    )
+    )*/
 
     // Option 2) Reactive design
-
+  this.beginnerCourses$ = http$.pipe(
+    map(courses => courses.filter(courses => courses.filter(course => course.category = 'BEGINNER')))
+  )
 
   }
 

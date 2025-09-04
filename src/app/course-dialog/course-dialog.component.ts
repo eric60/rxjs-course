72bf9f6a353df1e23061856dcfeda90c559b7017
Eric <ericshi98@gmail.com>
2025-09-04 08:21:45 -0400

    implement auto save feature - form draft pre-save

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -1,5 +1,5 @@
 import {AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild, ViewEncapsulation} from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import {Course} from "../model/course";
 import {FormBuilder, Validators, FormGroup} from "@angular/forms";
 import moment from 'moment';
@@ -9,56 +9,77 @@ import {fromPromise} from 'rxjs/internal-compatibility';
 import {Store} from '../common/store.service';
 
 @Component({
-    selector: 'course-dialog',
-    templateUrl: './course-dialog.component.html',
-    styleUrls: ['./course-dialog.component.css'],
-    standalone: false
+  selector: 'course-dialog',
+  templateUrl: './course-dialog.component.html',
+  styleUrls: ['./course-dialog.component.css'],
+  standalone: false
 })
 export class CourseDialogComponent implements AfterViewInit {
 
-    form: FormGroup;
+  form: FormGroup;
 
-    course:Course;
+  course: Course;
 
-    @ViewChild('saveButton', { static: true }) saveButton: ElementRef;
+  @ViewChild('saveButton', {static: true}) saveButton: ElementRef;
 
-    @ViewChild('searchInput', { static: true }) searchInput : ElementRef;
+  @ViewChild('searchInput', {static: true}) searchInput: ElementRef;
 
-    constructor(
-        private fb: FormBuilder,
-        private dialogRef: MatDialogRef<CourseDialogComponent>,
-        @Inject(MAT_DIALOG_DATA) course:Course,
-        private store:Store) {
+  constructor(
+    private fb: FormBuilder,
+    private dialogRef: MatDialogRef<CourseDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) course: Course,
+    private store: Store) {
 
-        this.course = course;
+    this.course = course;
 
-        this.form = fb.group({
-            description: [course.description, Validators.required],
-            category: [course.category, Validators.required],
-            releasedAt: [moment(), Validators.required],
-            longDescription: [course.longDescription,Validators.required]
-        });
+    this.form = fb.group({
+      description: [course.description, Validators.required],
+      category: [course.category, Validators.required],
+      releasedAt: [moment(), Validators.required],
+      longDescription: [course.longDescription, Validators.required]
+    });
 
-    }
+  }
 
-    ngAfterViewInit() {
+  ngOnInit() {
+    // value emitted is form inputs
+    this.form.valueChanges.pipe(
+      filter(() => this.form.valid)
+    ).subscribe(changes => {
 
-    }
+      const saveCourses$ = this.saveCourse(changes)
 
-    save() {
-        this.store.saveCourse(this.course.id, this.form.value)
-            .subscribe(
-                () => this.close(),
-                err => console.log("Error saving course", err)
-            );
-    }
+      saveCourses$.subscribe()
+    })
+  }
+
+  saveCourse(changes) {
+    return fromPromise(fetch(`/api/courses/${this.course.id}`,
+        {
+          method: 'PUT',
+          body: JSON.stringify(changes),
+          headers: {
+            'content-type': 'application/json'
+          }
+        }));
+  }
+
+  ngAfterViewInit() {
+
+  }
+
+  save() {
+    this.store.saveCourse(this.course.id, this.form.value)
+      .subscribe(
+        () => this.close(),
+        err => console.log("Error saving course", err)
+      );
+  }
 
 
-
-
-    close() {
-        this.dialogRef.close();
-    }
+  close() {
+    this.dialogRef.close();
+  }
 
 
 }
